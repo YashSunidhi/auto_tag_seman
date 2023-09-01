00c26018a51a8a5a8f02647817015f51df5536e7
YashSunidhi <smnitrkl50@gmail.com>
2023-09-01 12:48:21 +0200

    Update app.py

--- app.py
+++ app.py
@@ -276,84 +276,36 @@ if uploaded_file is not None:
     def page6():
         st.markdown("<h3 style='text-align: center; color: grey;'> Instruction Based Promotional Content Generation </h3>", unsafe_allow_html=True)
         #######
-        # define custom stopping criteria object
-        class StopOnTokens(StoppingCriteria):
-            def __call__(self, input_ids: torch.LongTensor, scores: torch.FloatTensor, **kwargs) -> bool:
-                for stop_ids in stop_token_ids:
-                    if torch.eq(input_ids[0][-len(stop_ids):], stop_ids).all():
-                        return True
-                return False
-
-        stopping_criteria = StoppingCriteriaList([StopOnTokens()])
-
-        ### Model Loading
-        model_id = 'meta-llama/Llama-2-13b-chat-hf'
-        #model_id = "conceptofmind/Yarn-Llama-2-13b-128k"
-
-        device = f'cuda:{cuda.current_device()}' if cuda.is_available() else 'cpu'
-
-        # set quantization configuration to load large model with less GPU memory
-        # this requires the `bitsandbytes` library
-        bnb_config = transformers.BitsAndBytesConfig(
-            load_in_4bit=True,
-            bnb_4bit_quant_type='nf4',
-            bnb_4bit_use_double_quant=True,
-            bnb_4bit_compute_dtype=bfloat16
-        )
-
+        
         # begin initializing HF items, you need an access token
-        hf_auth = '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'
-        model_config = transformers.AutoConfig.from_pretrained(
-            model_id,
-            use_auth_token=hf_auth
-        )
+        import requests
 
-        model = transformers.AutoModelForCausalLM.from_pretrained(
-            model_id,
-            trust_remote_code=True,
-            config=model_config,
-            quantization_config=bnb_config,
-            device_map='auto',
-            use_auth_token=hf_auth
-        )
+        API_URL = "https://api-inference.huggingface.co/models/meta-llama/Llama-2-13b-chat-hf"
+        headers = {"Authorization": "Bearer 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"}
 
-        # enable evaluation mode to allow model inference
-        model.eval()
-
-        tokenizer = transformers.AutoTokenizer.from_pretrained(
-            model_id,
-            use_auth_token=hf_auth
-        )
-
-        stop_list = ['\nHuman:', '\n```\n']
-        stop_token_ids = [tokenizer(x)['input_ids'] for x in stop_list]
-        stop_token_ids = [torch.LongTensor(x).to(device) for x in stop_token_ids]
-
-        generate_text = transformers.pipeline(
-            model=model,
-            tokenizer=tokenizer,
-            return_full_text=True,  # langchain expects the full text
-            task='text-generation',
-            # we pass model parameters here too
-            stopping_criteria=stopping_criteria,  # without this model rambles during chat
-            temperature=0.1,  # 'randomness' of outputs, 0.0 is the min and 1.0 the max
-            max_new_tokens=512,  # max number of tokens to generate in the output
-            repetition_penalty=1.1  # without this output begins repeating
-        )
+        def query(payload):
+            response = requests.post(API_URL, headers=headers, json=payload)
+            return response.json()
+            
+        output = query({
+            "inputs": "Can you please let us know more details about your ",
+        })
 
         # Get the input text from the user
         st.title("Marketing Compaign Generator")
         input_text = st.text_input("Enter the prompt/instruction for the newsletter:")
         prompt = input_text #"Generate a newsletter that captures the logical dependencies present in the provided context. Identify and highlight the relationships, cause-and-effect connections, and sequential progressions among different pieces of information. Ensure that the generated newsletter effectively communicates the flow of events, insights, or concepts by showcasing their interconnectedness and logical coherence if any from text with formal word included "  + str('""" ') + str(text) + str(' """')
 
-        prompt_template=f'''
+        prompt_template = f'''
 
         USER: {prompt}
 
         ASSISTANT: Write an impressive newsletter with Retrospectives and Prospectives finds for mentioned product. Please say Not Applicable if you are not confident or honest about outcome:
         '''
-        response=generate_text(prompt_template)
-        output_text = response[0]["generated_text"]
+        
+        output_text= query({
+            "inputs": prompt_template,
+        })
 
         # Get the input text from the user
         st.title("Generated Outcome")
