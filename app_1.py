30ad73f82c7b6bd4993f1fed1376c81465d76d3f
YashSunidhi <smnitrkl50@gmail.com>
2023-09-13 10:07:20 +0200

    Update app_1.py

--- app_1.py
+++ app_1.py
@@ -322,7 +322,7 @@ if uploaded_file is not None:
         option7 = st.text_input('Input your prompt here',"Write an executive short email for internal purposes based on document summary?")
         default_prompt = ["Create persuasive marketing content in " + option6 + " for " + option2+ ", emphasizing the " +option3+ " tone. Craft a "+ option4+ " that educates them about " + option1 +" role in cancer treatment and its potential benefits. The objective is to " + option5 + " to those seeking "+ option8+" options. The user-defined query is " + option7]
         #prompt = st.text_input('Input your prompt here')
-        prompt = st.write(default_prompt[0)
+        prompt = st.write(default_prompt[0])
         input_text = st.text_input("Write an executive short email for internal purposes based on document summary? ")
         
         dg_g = pd.read_csv(os.path.join(os.getcwd(),'Demo_lab_1 - Demo_lab.csv'))
