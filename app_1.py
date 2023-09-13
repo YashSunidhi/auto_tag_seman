18cbe2d1b104ce42c4a3c7eacc639f716de080c0
YashSunidhi <smnitrkl50@gmail.com>
2023-09-13 17:17:24 +0200

    Update app_1.py

--- app_1.py
+++ app_1.py
@@ -354,45 +354,6 @@ if uploaded_file is not None:
         
             #st.markdown('I make content on AI on regular basis do check my Youtube channel [link](https://www.youtube.com/@muhammadmoinfaisal/videos)')
     
-    
-            model_name_or_path = "TheBloke/Llama-2-7b-Chat-GPTQ"
-            # To use a different branch, change revision
-            # For example: revision="gptq-4bit-64g-actorder_True"
-            model = AutoModelForCausalLM.from_pretrained(model_name_or_path,
-                                                         torch_dtype=torch.float16,
-                                                         device_map="auto",
-                                                         revision="main")
-            
-            tokenizer = AutoTokenizer.from_pretrained(model_name_or_path, use_fast=True)
-            
-            prompt = "Tell me about AI"
-            prompt_template=f'''[INST] <<SYS>>
-            You are a helpful, respectful and honest assistant. Always answer as helpfully as possible, while being safe.  Your answers should not include any harmful, unethical, racist, sexist, toxic, dangerous, or illegal content. Please ensure that your responses are socially unbiased and positive in nature. If a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don't know the answer to a question, please don't share false information.
-            <</SYS>>
-            {prompt}[/INST]
-            
-            '''
-            
-            print("\n\n*** Generate:")
-            
-            input_ids = tokenizer(prompt_template, return_tensors='pt').input_ids.cuda()
-            output = model.generate(inputs=input_ids, temperature=0.7, max_new_tokens=512)
-            print(tokenizer.decode(output[0]))
-            
-            # Inference can also be done using transformers' pipeline
-            
-            print("*** Pipeline:")
-            pipe = pipeline(
-                "text-generation",
-                model=model,
-                tokenizer=tokenizer,
-                max_new_tokens=1024,
-                temperature=temperature,
-                top_p=top_p,
-                repetition_penalty=1.15
-            )
-            
-            #print(pipe(prompt_template)[0]['generated_text'])
         os.environ['REPLICATE_API_TOKEN']=add_replicate_api
         #Store the LLM Generated Reponese
         
@@ -421,7 +382,7 @@ if uploaded_file is not None:
                     default_system_prompt+="User: " + data["content"] + "\n\n"
                 else:
                     default_system_prompt+="Assistant" + data["content"] + "\n\n"
-            output=replicate.run(pipe, input={"prompt": f"{default_system_prompt} {prompt_input} Assistant: ",
+            output=replicate.run(llm, input={"prompt": f"{default_system_prompt} {prompt_input} Assistant: ",
                                              "temperature": temperature, "top_p":top_p, "max_length": max_length, "repititon_penalty":1.15})
         
             return output[0]['generated_text']
